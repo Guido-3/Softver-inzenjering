86e1eadfc3aa42c02f6dc9cd868d172257b391d8
Marko Gojkovic <marko.gojkovic123@gmail.com>
2024-05-30 16:12:44 +0200

    promjene

---
+++ citypass-frontend/src/app/services/sights.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SightsService {
+
+  private baseUrl = 'http://localhost:8080/citypass-api/znamenitost'; // URL vašeg backenda
+
+  constructor(private http: HttpClient) { }
+
+  getSights(): Observable<any[]> {
+    return this.http.get<any[]>(`${this.baseUrl}`);
+  }
+}
