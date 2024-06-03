8236093ab4b3fdc03d5559b48f5cf57f39f76630
Milica Bjelica <155689648+milicabj@users.noreply.github.com>
2024-06-03 15:23:09 +0200

    Add files via upload

---
+++ citypass-frontend/src/app/components/admin/sights/sights.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { SightsService } from 'src/app/services/sights.service';
+
+@Component({
+  selector: 'app-sights',
+  templateUrl: './sights.component.html',
+  styleUrls: ['./sights.component.scss']
+})
+export class SightsComponent implements OnInit{
+  znamenitosti: any = []
+
+  constructor(private sightsService: SightsService) { }
+
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+    this.sightsService.getSights().subscribe(
+      data => {
+        console.log('Data received:', data);
+        this.znamenitosti = data;
+      },
+      error => {
+        console.error('Error fetching sights data', error);
+      }
+    );
+  }
+}
