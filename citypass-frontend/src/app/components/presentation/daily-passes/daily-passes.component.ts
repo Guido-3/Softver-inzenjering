69f62ea967bf04be23aa6c08df67101d25519ae1
Milica Bjelica <155689648+milicabj@users.noreply.github.com>
2024-06-03 20:15:08 +0200

    Add files via upload

--- citypass-frontend/src/app/components/presentation/daily-passes/daily-passes.component.ts
+++ citypass-frontend/src/app/components/presentation/daily-passes/daily-passes.component.ts
@@ -7,7 +7,33 @@ import { SightsService } from 'src/app/services/sights.service';
   styleUrls: ['./daily-passes.component.scss']
 })
 export class DailyPassesComponent {
+  znamenitosti: any[] = [];
 
+  constructor(private sightsService: SightsService) { }
+
+  ngOnInit(): void {
+    console.log('ngOnInit called');
+    this.sightsService.getSights().subscribe(
+      data => {
+        console.log('Data received:', data);
+        this.znamenitosti = data;
+      },
+      error => {
+        console.error('Error fetching sights data', error);
+      }
+    );
+  }
+
+  
+  
+  icons = [
+    { class: 'fa-solid fa-building-columns', text: 'Museums' },
+    { class: 'class="fa-solid fa-wifi', text: 'WiFi' },
+    { class: 'fa-solid fa-fish', text: 'Aquarium' },
+    { class: 'fa-solid fa-cable-car', text: 'Cable Car' },
+    { class: 'fa-solid fa-bus-simple', text: 'Public transport' },
+    { class: 'fa-solid fa-archway', text: 'Fortress' }
+  ];
 }
 /**
  * import { Component } from '@angular/core';
